8dd5e6d4a0ced3eb16bc3f202a73a7e3b323bec2
Simon Stusak <simon.stusak@ifi.lmu.de>
2016-10-03 20:08:54 +0200

    first commit

---
+++ entertainment_center.py
@@ -0,0 +1,15 @@
+import media
+import fresh_tomatoes
+
+toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life", "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+avatar = media.Movie("Avatar", "A marine on an alien planet", "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg", "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+forrest_gump = media.Movie("Forrest Gump", "Man recalls his childhood in Greenbow, Alabama during the 1950s", "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg", "https://www.youtube.com/watch?v=uPIEn0M8su0")
+
+the_martian = media.Movie("The Martian", "Man left behind on Mars", "https://upload.wikimedia.org/wikipedia/en/c/cd/The_Martian_film_poster.jpg", "https://www.youtube.com/watch?v=ej3ioOneTy8")
+
+ex_machine = media.Movie("Ex Machina", "Man loves a robot", "https://images-na.ssl-images-amazon.com/images/M/MV5BMTUxNzc0OTIxMV5BMl5BanBnXkFtZTgwNDI3NzU2NDE@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://www.youtube.com/watch?v=XYGzRB4Pnq8")
+
+movies = [the_martian, ex_machine ,toy_story, avatar, forrest_gump]
+fresh_tomatoes.open_movies_page(movies)
