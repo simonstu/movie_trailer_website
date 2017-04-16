f351d9c5d29512ab52e4143b52f8c2489610d9a7
Simon Stusak <simon.stusak@ifi.lmu.de>
2017-04-16 22:47:11 +0200

    added changes according to review

--- media.py
+++ media.py
@@ -1,11 +1,14 @@
 import webbrowser
 
 class Movie():
+    """class for a movie"""
     def __init__(self, movie_title, movie_storyline, poster_img, trailer_youtube):
+        """a movie needs a title, storyline, url for the poster and url to a trailer on youtube"""
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_img
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """open a browser to show the trailer"""
         webbrowser.open(self.trailer_youtube_url)
